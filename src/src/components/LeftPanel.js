60e28a184e3fc3211b48935744a697b4dd093ddf
jeanlucasaocarvalho-blip <jeanlucas.a.o.carvalho@gmail.com>
2025-09-15 17:53:02 -0300

    Create LeftPanel.js

---
+++ src/src/components/LeftPanel.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LeftPanel() {
+  return (
+    <div className="left-panel">
+      <h2>Painel Esquerdo</h2>
+      <p>Aqui vai o menu de ferramentas do seu app.</p>
+    </div>
+  );
+}
+
+export default LeftPanel;
