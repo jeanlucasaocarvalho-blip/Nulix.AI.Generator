4aa50bf9623e7145c01e21f976e9aec79eab18a0
jeanlucasaocarvalho-blip <jeanlucas.a.o.carvalho@gmail.com>
2025-09-15 18:11:07 -0300

    Create constants.js

---
+++ src/components/src/components/src/services/src/src/public/src/src/src/components/src/components/src/components/src/components/src/constants.js
@@ -0,0 +1,7 @@
+export const RANDOM_PROMPTS = [
+  "Uma paisagem futurista ao pôr do sol.",
+  "Escreva um texto de motivação para quem está começando na programação.",
+  "Gere uma arte abstrata com tons de azul.",
+  "Crie um slogan para uma plataforma de IA inovadora.",
+  "Desenhe um robô simpático interagindo com humanos."
+];
